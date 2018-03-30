1bbf69f32c8140878b5ec3004bfb59eb4f247103
Shriroop <shriroop.joshi@gmail.com>
2018-03-30 18:21:53 -0500

    Error

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,4 +2,9 @@
 
 var router = require('express').Router();
 
+router.get('/location/:location', function(req, res) {
+    console.log('[HIT] location: ' + req.params.location);
+    res.send(200, 'Got location: ' + req.params.location);
+});
+
 module.exports = router;
